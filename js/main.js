6c5e8657c5044316b46fb8e77668a102156169df
sahilkhurana02 <sahil_khurana@outlook.com>
2016-12-24 01:01:42 +0530

    General Form completed

--- js/main.js
+++ js/main.js
@@ -2,10 +2,14 @@ var formNumber = 1;
 
 $("document").ready(function(){
     Materialize.updateTextFields();
+    $('select').material_select();
 	$(".next-button").on("click", function(){
 		$("#form" + formNumber).hide()
 	});
 	$(".previous-button").on("click", function(){
 		$("#form" + formNumber).show()
 	});
+
+
+	$('#generalForm').hide();
 });
