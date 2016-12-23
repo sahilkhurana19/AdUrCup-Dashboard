ab14e02f9a7ca531ba88b0e2b49faabff82b2ceb
sahilkhurana02 <sahil_khurana@outlook.com>
2016-12-24 01:47:46 +0530

    Added Switching Functionality

--- js/main.js
+++ js/main.js
@@ -3,13 +3,33 @@ var formNumber = 1;
 $("document").ready(function(){
     Materialize.updateTextFields();
     $('select').material_select();
+    
 	$(".next-button").on("click", function(){
-		$("#form" + formNumber).hide()
+		console.log("++")
+		if(formNumber < 2)
+			formNumber++;
+		Toggle(formNumber);
 	});
 	$(".previous-button").on("click", function(){
-		$("#form" + formNumber).show()
+		console.log("--")
+		if(formNumber > 1)
+			formNumber--;
+		Toggle(formNumber);
 	});
+	function Toggle(formNumber) {
+	switch(formNumber)
+	{
+		case 1: $("#generalForm").show();
+				$("#appearanceForm").hide();
+				break;
+
+		case 2: $("#appearanceForm").show();
+				$("#generalForm").hide(); 
+
+	}
+}
+	
 
 
-	$('#generalForm').hide();
+
 });
