1a1c010d44e19c09e251d157019c100735ffcaab
sahilkhurana02 <sahil_khurana@outlook.com>
2016-12-26 16:52:06 +0530

    Cleaned code for bootstrap migration

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
 $("document").ready(function(){
 	Materialize.updateTextFields();
-    $('select').material_select();
+    $("select").material_select();
+	
+    function displayVals() {
+  	var singleValues = $( "#cutlery" ).val();
+    console.log(singleValues);
+    if(singleValues === "Cup")
+    	$("#lidavail").prop("disabled", true);
+    
+}
+ 	
+	$( "#cutlery" ).change( displayVals );
+displayVals();
 
 })
